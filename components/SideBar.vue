a2424a0c6c008ac1c2f40644d3c43151245fca1c
Rys3t_ <kluh174@gmail.com>
2024-12-15 23:16:53 +0800

    調整樣式

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,39 +1,39 @@
 <template>
-    <aside
-        id="default-sidebar"
-        class="fixed top-0 left-0 z-50 w-64 h-full transition-transform duration-300 bg-gray-800 select-none md:translate-x-0"
-        :class="{
-            '-translate-x-full ': !bDrawerVisible,
-            'translate-x-0': bDrawerVisible
-        }">
-        <div class="h-full px-3 py-4">
-            <button @click="toggleDrawer" class="text-white">顯示</button>
-            <ul class="space-y-2 font-medium">
-                <li>
-                    <NuxtLink
-                        to="/"
-                        class="flex items-center p-2 text-white rounded-lg cursor-pointer hover:bg-gray-700">
-                        <span class="ml-3">About</span>
-                    </NuxtLink>
-                </li>
-            </ul>
-        </div>
-    </aside>
+  <aside
+    id="default-sidebar"
+    class="fixed left-0 top-0 z-50 h-full w-64 select-none bg-gray-800 transition-transform duration-300 md:translate-x-0"
+    :class="{
+      '-translate-x-full': !bDrawerVisible,
+      'translate-x-0': bDrawerVisible,
+    }"
+  >
+    <div class="h-full px-3 py-4">
+      <button @click="toggleDrawer" class="text-white">顯示</button>
+      <ul class="space-y-2 font-medium">
+        <li>
+          <NuxtLink to="/" class="flex cursor-pointer items-center rounded-lg p-2 text-white hover:bg-gray-700">
+            <span class="ml-3">About</span>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </aside>
 
-    <div
-        class="fixed inset-0 z-30 transition-opacity duration-300 bg-gray-900/50 backdrop-blur-md"
-        :class="{
-            'opacity-0': !bDrawerVisible,
-            'opacity-100': bDrawerVisible
-        }"
-        v-if="bDrawerVisible"
-        @click="toggleDrawer"></div>
+  <div
+    class="fixed inset-0 z-30 bg-gray-900/50 backdrop-blur-md transition-opacity duration-300"
+    :class="{
+      'opacity-0': !bDrawerVisible,
+      'opacity-100': bDrawerVisible,
+    }"
+    v-if="bDrawerVisible"
+    @click="toggleDrawer"
+  ></div>
 </template>
 
 <script setup>
-const bDrawerVisible = ref(false)
+const bDrawerVisible = ref(false);
 
 const toggleDrawer = () => {
-    bDrawerVisible.value = !bDrawerVisible.value
-}
+  bDrawerVisible.value = !bDrawerVisible.value;
+};
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: "2024-04-03",
-    devtools: {enabled: true},
-    modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-gtag"],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {}
-        }
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "nuxt-gtag"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-    colorMode: {
-        preference: "light",
-    },
-    gtag: {
-        id: 'G-H6P1897Y2J'
-      }
-    
-})
+  },
+  colorMode: {
+    preference: "light",
+  },
+  gtag: {
+    id: "G-H6P1897Y2J",
+    debug: true,
+  },
+});
